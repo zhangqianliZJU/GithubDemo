3b809cee9ab62a4e69a539f8743777f9ed260435
zhangqianli <zhangqianli414@163.com>
2018-04-04 15:19:44 +0800

    第二次提交

---
+++ Person.java
@@ -0,0 +1,23 @@
+public class Person {
+    private String name;
+    private int age;
+    private double height;
+
+    public Person(String name, int age, double height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+}
